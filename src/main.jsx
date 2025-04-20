2d65de39a36cbdf6d4f77657b7c14341d0558103
SarthakRoge <sarthakroge1920@gmail.com>
2025-04-20 22:08:14 +0530

    change map Loadscript

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,20 @@ import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './contexts/AuthContext';
 import './index.css';
 import { SubscriptionProvider } from './contexts/SubscriptionContext';
+import { LoadScript } from '@react-google-maps/api';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <SubscriptionProvider>
-        <Toaster position="top-center" />
-        <RouterProvider router={router} />
-      </SubscriptionProvider>
-    </AuthProvider>
+    <LoadScript
+      googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
+      libraries={['places']}
+    >
+      <AuthProvider>
+        <SubscriptionProvider>
+          <Toaster position="top-center" />
+          <RouterProvider router={router} />
+        </SubscriptionProvider>
+      </AuthProvider>
+    </LoadScript>
   </React.StrictMode>,
 );
